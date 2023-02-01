2175aaebe11eab115a8e2392bd07d39c075daa0e
mpyzara <mpyzara@gmail.com>
2023-02-01 16:18:49 +0100

    +1

--- install-docker.sh
+++ install-docker.sh
@@ -1,3 +1,4 @@
+ sudo apt-get remove docker docker-engine docker.io containerd runc
  sudo apt-get update
  sudo apt-get install \
     ca-certificates \
@@ -11,7 +12,7 @@ sudo mkdir -p /etc/apt/keyrings
  echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  
+
   sudo apt-get update
 
   sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
